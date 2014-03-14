e9c381297360a3ab1fb8630e96f149e2d701441c
Vilachao <josemaria1986@gmail.com>
2014-03-14 14:31:24 +0100

    Actualizacion2

--- SSdiasTableViewController.m
+++ SSdiasTableViewController.m
@@ -39,8 +39,7 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-#warning Incomplete method implementation.
-    // Return the number of rows in the section.
+
     return 0;
 }
 
